bbd9e13803b3ee29a7123f1cb7a64fbf8703a510
Joshua Jeneral <jjeneralr.externo@sii.cl>
2024-06-17 00:36:01 -0400

    fix: Se cambia a relación unidireccional entre User y Phone ya que se estaba insertando null en el campo de unión

--- inadvance-challenge/src/main/java/com/jjeneral/inadvancechallenge/model/entity/Phone.java
+++ inadvance-challenge/src/main/java/com/jjeneral/inadvancechallenge/model/entity/Phone.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -30,9 +28,5 @@ public class Phone {
   private Integer number;
   private Byte citycode;
   private Short countrycode;
-
-  @ManyToOne
-  @JoinColumn(name = "user_id")
-  private User user;
 
 }
--- inadvance-challenge/src/main/java/com/jjeneral/inadvancechallenge/model/entity/User.java
+++ inadvance-challenge/src/main/java/com/jjeneral/inadvancechallenge/model/entity/User.java
@@ -7,6 +7,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -43,7 +44,8 @@ public class User {
     private String email;
     private String password;
 
-    @OneToMany(mappedBy = "user", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @OneToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private List<Phone> phones = new ArrayList<>();
 
     @CreatedDate
